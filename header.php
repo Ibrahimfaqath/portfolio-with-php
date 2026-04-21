0b0bdddf55c5316b22e169b57c0e2bd4f14d1047
Ibrahimfaqath <ibrahimfaqath@gmail.com>
2026-04-21 13:08:55 +0700

    add navbar

---
+++ header.php
@@ -0,0 +1,8 @@
+<nav class="navbar">
+    <p>Ibrahim</p>
+    <a href="index.php">Home</a>
+    <a href="">About Me</a>
+    <a href="">My Portfolio</a>
+    <button>Contact Me</button>
+</nav>
+   
